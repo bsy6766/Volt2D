0b4816bea3a1ea893296d2b5a0cd4fa240abb3d5
Seung Youp Baek <bsy6766@wireless-10-147-29-162.public.utexas.edu>
2015-01-23 09:55:43 -0600

    Created Progress bar class

---
+++ OpenGL_2D_Framework/ProgressBar.cpp
@@ -0,0 +1,9 @@
+//
+//  ProgressBar.cpp
+//  OpenGL_2D_Framework
+//
+//  Created by Seung Youp Baek on 1/23/15.
+//  Copyright (c) 2015 Seung Youp Baek. All rights reserved.
+//
+
+#include "ProgressBar.h"
---
+++ OpenGL_2D_Framework/ProgressBar.h
@@ -0,0 +1,14 @@
+//
+//  ProgressBar.h
+//  OpenGL_2D_Framework
+//
+//  Created by Seung Youp Baek on 1/23/15.
+//  Copyright (c) 2015 Seung Youp Baek. All rights reserved.
+//
+
+#ifndef __OpenGL_2D_Framework__ProgressBar__
+#define __OpenGL_2D_Framework__ProgressBar__
+
+#include <stdio.h>
+
+#endif /* defined(__OpenGL_2D_Framework__ProgressBar__) */
