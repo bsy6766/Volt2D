3e503db559703e154aee7d7d668f91b91f7a0201
Seung Youp Baek <bsy6766@Seungs-MacBook-Pro.local>
2015-02-03 11:59:53 -0600

    added Director class

---
+++ OpenGL_2D_Framework/Director.cpp
@@ -0,0 +1,9 @@
+//
+//  Director.cpp
+//  OpenGL_2D_Framework
+//
+//  Created by Seung Youp Baek on 2/3/15.
+//  Copyright (c) 2015 Seung Youp Baek. All rights reserved.
+//
+
+#include "Director.h"
---
+++ OpenGL_2D_Framework/Director.h
@@ -0,0 +1,14 @@
+//
+//  Director.h
+//  OpenGL_2D_Framework
+//
+//  Created by Seung Youp Baek on 2/3/15.
+//  Copyright (c) 2015 Seung Youp Baek. All rights reserved.
+//
+
+#ifndef __OpenGL_2D_Framework__Director__
+#define __OpenGL_2D_Framework__Director__
+
+#include <stdio.h>
+
+#endif /* defined(__OpenGL_2D_Framework__Director__) */
