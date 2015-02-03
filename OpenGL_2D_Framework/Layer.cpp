0bb6d4133be765046025d32ea1e6b086080f80d2
Seung Youp Baek <bsy6766@Seungs-MacBook-Pro.local>
2015-02-03 11:46:16 -0600

    added layer class

---
+++ OpenGL_2D_Framework/Layer.cpp
@@ -0,0 +1,9 @@
+//
+//  Layer.cpp
+//  OpenGL_2D_Framework
+//
+//  Created by Seung Youp Baek on 2/3/15.
+//  Copyright (c) 2015 Seung Youp Baek. All rights reserved.
+//
+
+#include "Layer.h"
---
+++ OpenGL_2D_Framework/Layer.h
@@ -0,0 +1,14 @@
+//
+//  Layer.h
+//  OpenGL_2D_Framework
+//
+//  Created by Seung Youp Baek on 2/3/15.
+//  Copyright (c) 2015 Seung Youp Baek. All rights reserved.
+//
+
+#ifndef __OpenGL_2D_Framework__Layer__
+#define __OpenGL_2D_Framework__Layer__
+
+#include <stdio.h>
+
+#endif /* defined(__OpenGL_2D_Framework__Layer__) */
