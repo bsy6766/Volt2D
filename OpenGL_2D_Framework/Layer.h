d5587f49694f82bbc2ebbfcb4de96c1e8b5977b7
Seung Youp Baek <bsy6766@Seungs-MacBook-Pro.local>
2015-02-03 12:00:24 -0600

    building...

--- OpenGL_2D_Framework/Layer.h
+++ OpenGL_2D_Framework/Layer.h
@@ -9,6 +9,25 @@
 #ifndef __OpenGL_2D_Framework__Layer__
 #define __OpenGL_2D_Framework__Layer__
 
-#include <stdio.h>
+#include "Sprite.h"
+#include "SpriteManager.h"
+#include "ParticleSystem.h"
+#include "ParticleSystemManager.h"
+
+class Layer{
+protected:
+    int z;  //z depth. Used for layers only.
+private:
+    
+public:
+    Layer();                    //constructor
+    Layer(const Layer& other);  //copy constructor
+    
+    /**
+     *  Add sprite to the layer
+     */
+    void addSprite(Sprite* sprite);
+    void addParticle(ParticleSystem* particleSystem);
+};
 
 #endif /* defined(__OpenGL_2D_Framework__Layer__) */
