c2cd919abeb158005774b332b5bdb1ac004c0f78
bsy6766 <bsy6766@gmail.com>
2015-06-15 10:02:14 -0500

    removing actiontype. adding action fade by

---
+++ OpenGL_2D_Framework/ActionFadeBy.cpp
@@ -0,0 +1,9 @@
+//
+//  ActionFadeBy.cpp
+//  OpenGL_2D_Framework
+//
+//  Created by Seung Youp Baek on 6/11/15.
+//  Copyright (c) 2015 Seung Youp Baek. All rights reserved.
+//
+
+#include "ActionFadeBy.h"
---
+++ OpenGL_2D_Framework/ActionFadeBy.h
@@ -0,0 +1,14 @@
+//
+//  ActionFadeBy.h
+//  OpenGL_2D_Framework
+//
+//  Created by Seung Youp Baek on 6/11/15.
+//  Copyright (c) 2015 Seung Youp Baek. All rights reserved.
+//
+
+#ifndef __OpenGL_2D_Framework__ActionFadeBy__
+#define __OpenGL_2D_Framework__ActionFadeBy__
+
+#include <stdio.h>
+
+#endif /* defined(__OpenGL_2D_Framework__ActionFadeBy__) */
