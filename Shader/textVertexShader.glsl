fd8ff9ca62968908c04b6d22359952ed1aa88629
bsy6766 <bsy6766@gmail.com>
2015-07-06 04:23:52 -0500

    Changed text vertex shader. Added char's translate mat

--- Shader/textVertexShader.glsl
+++ Shader/textVertexShader.glsl
@@ -2,18 +2,21 @@
 
 layout(location = 0) in vec3 vert;
 layout(location = 1) in vec2 uvVert;
+//layout(location = 2) in vec2 charTransVert;
 
 uniform mat4 modelMat;
 uniform mat4 translateMat;
 uniform mat4 rotateMat;
 uniform mat4 scaleMat;
 uniform mat4 cameraMat;
+uniform mat4 charTransMat;
 
 out vec2 fragTexCoord;
 
 void main(){
     vec4 finalPosition = vec4(vert, 1.0);
-    mat4 objectMat = modelMat * translateMat * rotateMat * scaleMat;
+    mat4 objectMat = modelMat * translateMat * rotateMat * scaleMat * charTransMat;
+//    mat4 objectMat = modelMat * translateMat * rotateMat * scaleMat;
     gl_Position = cameraMat * objectMat * finalPosition;
     fragTexCoord = uvVert;
 }
