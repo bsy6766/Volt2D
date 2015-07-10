04afd1645e83a6b119c53524f807dddc15d7c48f
bsy6766 <bsy6766@gmail.com>
2015-07-10 03:44:57 -0500

    cleaned

--- OpenGL_2D_Framework/main.cpp
+++ OpenGL_2D_Framework/main.cpp
@@ -8,13 +8,10 @@
 
 #include <iostream>
 #include <string>
-//#include "GameManager.h"
 #include "Director.h"
 #include "TitleScene.h"
 
 #include "SpriteSheet.h"
-
-
 
 int main(int argc, const char * argv[]) {
     
@@ -31,75 +28,24 @@ int main(int argc, const char * argv[]) {
     std::string runningPath(cCurrentPath);
     std::cout << "The current working directory is " << runningPath << std::endl;
     
-    //char buffer[1024];
-    //char *answer = getcwd(buffer, sizeof(buffer));
-    //string s_cwd;
-    //if (answer)
-    //{
-    //    s_cwd = answer;
-    //}
-    //
-    //std::cout << "The current working directory is " << s_cwd << std::endl;
-    
-    
     std::string windowTitle = "opengl 2d framework";
     
     SpriteSheet* ss = new SpriteSheet();
     ss->initSpriteSheetWithXML("battle scene.xml");
     delete ss;
     
-    
-//    GameManager* gm = new GameManager();
     Director::getInstance().setWindowSize(1280, 720);
     
     try{
-        Director::getInstance().initApp(1280, 720, "OpenGL");
+        Director::getInstance().initApp(1280, 720, "JB_Engine(OpenGL4) test game build");
         TitleScene* titleScene = new TitleScene();
         Director::getInstance().pushScene(titleScene);
         Director::getInstance().run();
-//        gm->initEssentials();
-//        
-//        //init the game
-//        gm->initGLFW();
-//        gm->createWindow(1280, 720, windowTitle);
-//        
-//        
-//        gm->initGLEW();
-//        gm->initOpenGL();
-//        
-//        //load shaders
-//        gm->loadShader("../Shader/vertexShader.glsl", GL_VERTEX_SHADER);
-//        gm->loadShader("../Shader/fragmentShader.glsl", GL_FRAGMENT_SHADER);
-//        
-//        //create program
-//        gm->loadProgram();
-//        
-//        gm->loadCamera();
-//        
-//        gm->initSpriteManager();
-//        
-//        gm->initParticleSystemManager();
-//        
-//        gm->initParticleSystem();
-//        
-//        gm->initLoading();
-//        
-//        gm->startGame();
+
     }
     catch(std::exception &e){
         std::cout << e.what() << std::endl;
     }
-    
-    //exit app
-//    Director::getInstance().terminateApp();
-//    gm->releaseProgram();
-//    gm->releaseShader();
-//    gm->releaseUndeadActions();
-//    gm->releaseSpriteManager();
-//    gm->releaseParticleSystemManager();
-//    gm->releaseCamera();
-//    
-//    delete gm;
     
     return 0;
 }
