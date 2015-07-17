23322d310b4415f7f6316d92d19bd6920ae1e21c
bsy6766 <bsy6766@gmail.com>
2015-07-17 11:34:38 -0500

    random commit

--- StatusBar.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  StatusBar.cpp
-//  OpenGL_2D_Framework
-//
-//  Created by Seung Youp Baek on 7/14/15.
-//  Copyright (c) 2015 Seung Youp Baek. All rights reserved.
-//
-
-#include "StatusBar.h"
