32d455eb636e610396b55cb61fd2a1e4199b647e
bsy6766 <bsy6766@gmail.com>
2015-07-17 23:49:31 -0500

    Moved bunch of data from RenderableObject. Moved RenderableObjectManager feature to here.

--- OpenGL_2D_Framework/Object.cpp
+++ OpenGL_2D_Framework/Object.cpp
@@ -8,27 +8,164 @@
 
 #include "Object.h"
 
-Object::Object()
-:size(Director::getInstance().getWindowSize()),
-position(glm::vec3(0, 0, 0))
+Object::Object():
+position(glm::vec3(0, 0, 0)),
+parent(0),
+needToUpdateBB(false),
+boundingBox(0),
+translateMat(glm::mat4()),
+rotateMat(glm::mat4()),
+scaleMat(glm::mat4()),
+modelMat(glm::mat4()),
+angle(0),
+scale(glm::vec3(1, 1, 1))
 {
     cout << "Object::Object()" << endl;
 }
 
 Object::~Object(){
     cout << "Object::~Object()" << endl;
+    cleanChildList();
+}
+
+void Object::cleanChildList(){
+    for(auto it : childObjMap){
+        delete (it.second);
+    }
+    //LUT has same pointer with obj map.
 }
 
 void Object::setPosition(glm::vec3 position){
     this->position = position;
+    translateTo(position);
 }
 
 void Object::addPosition(glm::vec3 position){
-    this-> position += position;
+    this->position += position;
+    translateBy(position);
 }
 
-glm::vec3 Object::getPosition(){
+void Object::setX(float x){
+    this->position.x = x;
+    translateTo(this->position);
+}
+
+void Object::setY(float y){
+    this->position.y = y;
+    translateTo(this->position);
+}
+
+const glm::vec3 Object::getPosition(){
     return this->position;
+}
+
+
+void Object::translateTo(glm::vec3 position){
+    glm::vec3 scaledPos = glm::vec3(position.x / 10, position.y / 10, position.z / 10);
+    translateMat = glm::translate(glm::mat4(), scaledPos);
+    needToUpdateBB = true;
+    
+//    updateModelMat();
+}
+
+void Object::translateBy(glm::vec3 distance){
+    glm::vec3 scaledDistance = glm::vec3(distance.x / 10, distance.y / 10, distance.z / 10);
+    translateMat = glm::translate(translateMat, scaledDistance);
+    needToUpdateBB = true;
+    
+//    updateModelMat();
+}
+
+void Object::setAngle(GLfloat angle, glm::vec3 axis){
+    angle *= (-1);
+    rotateTo(angle, axis);
+}
+
+void Object::addAngle(GLfloat angle, glm::vec3 axis){
+    angle *= (-1);
+    rotateBy(angle, axis);
+}
+
+void Object::wrapAngle(GLfloat& angle){
+    if(angle < 0)
+        angle += 360;
+    else if(angle > 360)
+        angle -= 360;
+    else if(angle == 360)
+        angle = 0;
+}
+
+const GLfloat Object::getAngle(){
+    return this->angle;
+}
+
+void Object::rotateTo(GLfloat angle, glm::vec3 axis = glm::vec3(0, 0, 1)){
+    //rotate in 2d space by default
+    wrapAngle(angle);
+    rotateMat = glm::rotate(glm::mat4(), angle, axis);
+    this->angle = angle;
+    needToUpdateBB = true;
+    
+//    updateModelMat();
+}
+
+void Object::rotateBy(GLfloat angle, glm::vec3 axis = glm::vec3(0, 0, 1)){
+    //rotate in 2D space by default
+    this->angle += angle;
+    wrapAngle(this->angle);
+    rotateMat = glm::rotate(rotateMat, angle, axis);
+    needToUpdateBB = true;
+    
+//    updateModelMat();
+}
+
+const glm::vec3 Object::getScale(){
+    return this->scale;
+}
+
+const GLfloat Object::getScaleX(){
+    return scale.x;
+}
+
+const GLfloat Object::getScaleY(){
+    return scale.y;
+}
+
+const GLfloat Object::getScaleZ(){
+    return scale.z;
+}
+
+void Object::setScale(glm::vec3 scale){
+    scaleTo(scale);
+}
+
+void Object::addScale(glm::vec3 scale){
+    scaleBy(scale);
+}
+
+void Object::scaleTo(glm::vec3 scale){
+    this->scale = scale;
+    scaleMat = glm::scale(glm::mat4(), scale);
+    needToUpdateBB = true;
+    
+//    updateModelMat();
+}
+
+void Object::scaleBy(glm::vec3 scale){
+    this->scale += scale;
+    scaleMat = glm::scale(glm::mat4(), this->scale);
+    needToUpdateBB = true;
+    
+//    updateModelMat();
+}
+
+glm::mat4 Object::getTransformMat(){
+    if(parent){
+        return this->parent->getTransformMat() * translateMat * rotateMat * scaleMat;
+    }
+    else{
+        return translateMat * rotateMat * scaleMat;
+    }
 }
 
 std::string Object::getName(){
@@ -36,5 +173,187 @@ std::string Object::getName(){
 }
 
 void Object::setName(std::string objectName){
+    //TODO: reject empty string
     this->objectName = objectName;
+}
+
+void Object::setParent(Object *parent){
+    this->parent = parent;
+}
+
+Object* Object::getParent(){
+    return this->parent;
+}
+
+bool Object::addChild(Object *child, Object *parent, bool replace){
+    child->setParent(parent);
+    
+    auto obj_it = childObjectLUT.find(child->objectName);
+    
+    //LUT
+    if(obj_it == childObjectLUT.end()){
+        //no object exists with same name. remove
+        cout << "Adding object named \"" << child->objectName << "\" to system." << endl;
+    }
+    else{
+        if(replace){
+            cout << "Replacing existing object named \"" << child->objectName << "\"." << endl;
+            //remove exsiting name on LUT
+            childObjectLUT.erase(obj_it);
+            //remove from manager
+        }
+        else{
+        }
+    }
+    //add to manager and add to LUT
+    childObjectLUT.insert(std::pair<std::string, Object*>(child->objectName, child));
+    
+    float objectZ;
+    //get Z. If fails, set z as +1 from highest z to render to the most top of scene
+    if(!child->getZDepth(objectZ)){
+        if(childObjMap.empty()){
+            objectZ = 0;
+        }
+        else{
+            auto end_it = childObjMap.end();
+            end_it--;
+            float lastZ = end_it->first;
+            objectZ = lastZ + 1;
+            //assign z value because it didn't had one
+            child->setZDepth(objectZ);
+        }
+    }
+//    if(scenePtr)
+//        child->bindScene(scenePtr);
+    childObjMap.insert(std::pair<float, Object*>(objectZ, child));
+    
+    return true;
+}
+
+void Object::updateChild(){
+    for (auto it = childObjMap.begin(); it != childObjMap.end();){
+        //if pointer is null, delete pointer and remove from the list.
+        if(it->second == nullptr){
+            delete (it->second);
+            childObjMap.erase(it);
+        }
+        else{
+            //first, update it self so child can have updated parent.
+            (it->second)->update();
+            //update child's child
+            (it->second)->updateChild();
+            ++it;
+        }
+    }
+}
+
+void Object::renderChild(glm::mat4 parentModelMat){
+    for (auto it = childObjMap.begin(); it != childObjMap.end();){
+        //if pointer is null, delete pointer and remove from the list.
+        if(it->second == nullptr){
+            delete (it->second);
+            childObjMap.erase(it);
+        }
+        else{
+            //update model Mat
+            modelMat = parentModelMat * glm::mat4();
+            //first render itself
+            (it->second)->render();
+            //then render child
+//            (it->second)->renderChild(this->modelMat);
+            ++it;
+        }
+    }
+    
+    //omg double iteration. so bad!
+    for (auto it = childObjMap.begin(); it != childObjMap.end();){
+        //if pointer is null, delete pointer and remove from the list.
+        if(it->second == nullptr){
+            delete (it->second);
+            childObjMap.erase(it);
+        }
+        else{
+            //update model Mat
+            modelMat = parentModelMat * glm::mat4();
+            //first render itself
+//            (it->second)->render();
+            //then render child
+            (it->second)->renderChild(this->modelMat);
+            ++it;
+        }
+    }
+}
+
+BoundingBox* const Object::getBoundingBox(){
+    if(this->boundingBox && needToUpdateBB)
+        this->boundingBox->updateBoundingBox(
+                                             glm::translate(glm::mat4(), this->position),
+                                             this->scaleMat,
+                                             this->rotateMat);
+    needToUpdateBB = false;
+    return this->boundingBox;
+}
+
+void Object::setZDepth(float z){
+    //if this object already had z value, then change in the manager
+    if(this->z_depth.dirty){
+        //use scene that is bound to this object to call renderable manager
+        this->changeZ(this, z);
+    }
+    //else, change it self.
+    else{
+        this->z_depth.dirty = true;
+        this->z_depth.z = z;
+    }
+}
+
+bool Object::getZDepth(float& z){
+    if(this->z_depth.dirty){
+        z = this->z_depth.z;
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool Object::isZValid(){
+    return this->z_depth.dirty;
+}
+
+//This function is only called when z_depth has been set. No need to check if it's none
+void Object::changeZ(Object *object, float z){
+    //check if z is valid
+    if(object->isZValid()){
+        //if so, check if it can get z
+        float curZ;
+        if(object->getZDepth(curZ)){
+            //check if object is on LUT
+            std::string name = object->getName();
+            auto nameIt = childObjectLUT.find(name);
+            if(nameIt != childObjectLUT.end()){
+                //check if
+                auto changingIt = childObjMap.find(curZ);
+                if(changingIt != childObjMap.end()){
+                    childObjMap.erase(changingIt);
+                    //can be vulnerable.
+                    object->z_depth.z = z;
+                    childObjMap.insert(std::pair<float, Object*>(z, object));
+                }
+                else{
+                    cout << "object not found in system" << endl;
+                }
+            }
+            else{
+                cout << "Object name not found in LUT" << endl;
+            }
+        }
+        else{
+            cout << "Z_Depth ERROR: Failed to get z depth value" << endl;
+        }
+    }
+    else{
+        //this object doens't have
+        cout << "Z_Deprth ERROR: z_depth value not found for this object" << endl;
+    }
 }
--- OpenGL_2D_Framework/Object.h
+++ OpenGL_2D_Framework/Object.h
@@ -11,28 +11,209 @@
 
 //#define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
-#include "Director.h"
+#include <GL/glew.h>
+#include <map>
 #include "CommonInclude.h"
+#include "BoundingBox.h"
+#include "Z_Float.h"
+
+/**
+ *  \class Object
+ *  
+ *  \brief The most basic state of class that can be exist.
+ *
+ *  This class is the highest base class. 
+ *  All object that exists in system will inherite this class.
+ *  Object class itself has no rendering feature.
+ *  However, it will have position, boundingbox and transform matrixes
+ *  to exists as an virtual object in the scene. (like invisible tracking node)
+ *  To actually render the object, you will have to use RenderableObject.
+ */
 
 class Object{
 protected:
+    //global z depth
+    Z_Float z_depth;
+    
+    //Position
     glm::vec3 position;
-    WinSize size;
+    
+    //Naming
     std::string objectName;
     
+    //BoundingBox
+    BoundingBox* boundingBox;
+    bool needToUpdateBB;
+    
+    //Childing
+    Object* parent;
+    
+    //OpenGL Matrix
+    glm::mat4 translateMat;
+    glm::mat4 rotateMat;
+    glm::mat4 scaleMat;
+    glm::mat4 modelMat;
+    
+    //0 ~ 360 degree
+    GLfloat angle;
+    //x,y,z. -1.0 ~ 1.0
+    glm::vec3 scale;
+    
+    //Childing
+    bool addChild(Object* child, Object* parent, bool replace = false);
+    //TODO: removeObject
+    void updateChild();
+    void renderChild(glm::mat4 parentModelMat);
+    
+    /**
+     *  Translate to position
+     *  @param position A position to translate object
+     */
+    void translateTo(glm::vec3 position);
+    
+    /*
+     *
+     */
+    void translateBy(glm::vec3 distance);
+    
+    //To transformation
+    
+    void wrapAngle(GLfloat& angle);
+    void rotateTo(GLfloat angle, glm::vec3 axis);
+    void scaleTo(glm::vec3 scale);
+    
+    //By transformation
+    void rotateBy(GLfloat angle, glm::vec3 axis);
+    void scaleBy(glm::vec3 scale);
+    
+    //Child mapping
+    std::multimap<std::string, Object*> childObjectLUT; //look up table
+    std::multimap<float, Object*> childObjMap;
+    
 private:
+    void cleanChildList();
+    
+    void setParent(Object* parent);
+    Object* getParent();
+    
+    //From RenderableObjectManager
+//    bool addObject(Scene* scenePtr, std::string objectName, RenderableObject* object, bool replace);
+//    bool removeObject(RenderableObject* object, bool deleteObject);
+//    bool removeObject(std::string objectName);
+    void changeZ(Object* object, float z);
+    void changeZDepth(float z);
 public:
     Object();
     virtual ~Object();
     
-    //position
+    glm::mat4 getTransformMat();
+    
+    /// @{
+    /// @name Setters and Getters for Position, Angle, and Scale
+    /**
+     *  Set position
+     *  @param position A new vec3 position to overwrite current object's position
+     */
     void setPosition(glm::vec3 position);
+    
+    /**
+     *  Add position
+     *  @param position A new vec3 position to add on current position of object
+     */
     void addPosition(glm::vec3 position);
-    glm::vec3 getPosition();
+    
+    /**
+     *  Get position
+     *  @return (const) Get object's current position
+     */
+    const glm::vec3 getPosition();
+    
+    /**
+     *  Set x position of object
+     *  @param x A value of x position to set
+     */
+    void setX(float x);
+    
+    /**
+     *  Set y position of object
+     *  @param y A value of y position to set
+     */
+    void setY(float y);
+    
+    /**
+     *  Set Angle
+     *  @param angle An angle in degree to overwrite object's current angle
+     *  @param axis An rotation axis. Set to Z-Axis by default.
+     */
+    void setAngle(GLfloat angle, glm::vec3 axis = glm::vec3(0, 0, 1));
+    
+    /**
+     *  Add angle
+     *  @param angle An angle in degree to add on current angle of object
+     *  @param axis An rotation axis. Set toZ-Axis by default.
+     */
+    void addAngle(GLfloat angle, glm::vec3 axis = glm::vec3(0, 0, 1));
+    
+    /**
+     *  Get angle
+     *  @Return (const) A float value of current angle of object in degree
+     */
+    const GLfloat getAngle();
+    
+    /**
+     *  Set scale
+     *  @note 1.0 is a default scale.
+     *  @param A vec3 scale value to overwrite object's current scale
+     */
+    void setScale(glm::vec3 scale);
+    
+    /**
+     *  Add scale
+     *  @note 1.0 is a default scale.
+     *  @param A vec3 scale value to add on object's current scale
+     */
+    void addScale(glm::vec3 scale);
+    
+    /**
+     *  Get scale
+     *  @return (const) A vec3 scale value of object
+     */
+    const glm::vec3 getScale();
+    
+    /**
+     *  Get scale x value
+     *  @return (const) A float x value of scale
+     */
+    const GLfloat getScaleX();
+    
+    /**
+     *  Get scale y value
+     *  @return (const) A float y value of scale
+     */
+    const GLfloat getScaleY();
+    
+    /**
+     *  Get scale z value
+     *  @return (const) A float z value of scale
+     */
+    const GLfloat getScaleZ();
+    /// @}
     
     //name
     std::string getName();
     void setName(std::string objectName);
+    
+    //bounding box
+    BoundingBox* const getBoundingBox();
+    
+    //z depth
+    void setZDepth(float z);
+    bool getZDepth(float& z);
+    bool isZValid();
+    
+    //virtual. 
+    virtual void render() = 0;
+    virtual void update() = 0;
 };
 
 #endif /* defined(__OpenGL_2D_Framework__Object__) */
