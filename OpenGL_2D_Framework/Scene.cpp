87793c95b148b16f95356faaab129d61ae7d85f6
bsy6766 <bsy6766@gmail.com>
2015-07-17 23:47:20 -0500

    removed layer map. updates child. a bit more expensive key calling downward.

--- OpenGL_2D_Framework/Scene.cpp
+++ OpenGL_2D_Framework/Scene.cpp
@@ -10,14 +10,15 @@
 #include <GLFW/glfw3.h>
 
 Scene::Scene():
+Object(),
 window(0),
-renderableObjectManager(0),
+//renderableObjectManager(0),
 prevMousePos(glm::vec2()),
 curMousePos(glm::vec2()),
 canMoveCamera(false)
 {
     cout << "Scene()" << endl;
-    renderableObjectManager = new RenderableObjectManager();
+//    renderableObjectManager = new RenderableObjectManager();
 }
 
 Scene::~Scene(){
@@ -25,7 +26,7 @@ Scene::~Scene(){
     exit();
 }
 
-void Scene::boundWindow(GLFWwindow *window){
+void Scene::bindWindow(GLFWwindow *window){
     this->window = window;
     double x, y;
     glfwGetCursorPos(window, &x, &y);
@@ -35,21 +36,11 @@ void Scene::boundWindow(GLFWwindow *window){
 }
 
 void Scene::update(){
-    renderableObjectManager->update();
-    
-    //update layers
-    for(auto it : layerMap){
-        (it.second)->update();
-    }
+    this->updateChild();
 }
 
 void Scene::render(){
-    renderableObjectManager->render();
-    
-    //render layers
-    for(auto it : layerMap){
-        (it.second)->render();
-    }
+    this->renderChild(this->modelMat);
 }
 
 void Scene::run(){
@@ -58,71 +49,126 @@ void Scene::run(){
     render();
 }
 
-void Scene::addObject(std::string objectName, RenderableObject* object){
-    renderableObjectManager->addObject(this, objectName, object, false);
+void Scene::addChild(Object *child){
+    this->Object::addChild(child, this);
 }
 
-void Scene::addLayer(Layer *childLayer){
-    float layerZ;
-    //get Z. If fails, set z as +1 from highest z to render to the most top of scene
-    if(!childLayer->getZorder(layerZ)){
-        if(layerMap.empty()){
-            layerZ = 0;
-        }
-        else{
-            auto end_it = layerMap.end();
-            end_it--;
-            float lastZ = end_it->first;
-            layerZ = lastZ + 1;
-            //assign z value because it didn't had one
-            childLayer->setZorder(layerZ);
-        }
-    }
-    childLayer->init();
-    layerMap.insert(std::pair<int, Layer*>(layerZ, childLayer));
-}
+//void Scene::addObject(std::string objectName, RenderableObject* object){
+//    renderableObjectManager->addObject(this, objectName, object, false);
+//}
+//
+//void Scene::addLayer(Layer *childLayer){
+//    float layerZ;
+//    //get Z. If fails, set z as +1 from highest z to render to the most top of scene
+//    if(!childLayer->getZorder(layerZ)){
+//        if(layerMap.empty()){
+//            layerZ = 0;
+//        }
+//        else{
+//            auto end_it = layerMap.end();
+//            end_it--;
+//            float lastZ = end_it->first;
+//            layerZ = lastZ + 1;
+//            //assign z value because it didn't had one
+//            childLayer->setZorder(layerZ);
+//        }
+//    }
+//    childLayer->init();
+//    layerMap.insert(std::pair<int, Layer*>(layerZ, childLayer));
+//}
 
 void Scene::exit(){
-    if(renderableObjectManager)
-        delete renderableObjectManager;
-    
-    for(auto it : layerMap){
-        delete it.second;
-    }
-}
-
-RenderableObjectManager* Scene::getRenderableObjectManager(){
-    return renderableObjectManager;
 }
 
 void Scene::keyPressed(int key, int mods){
-    for(auto it : layerMap){
-        if((it.second)->isLayerInputListenable()){
-			(it.second)->keyPressed(key, mods);
+//    for(auto it : layerMap){
+//        if((it.second)->isLayerInputListenable()){
+//			(it.second)->keyPressed(key, mods);
+//        }
+    //    }
+    for (auto it = this->childObjMap.begin(); it != this->childObjMap.end();){
+        //if pointer is null, delete pointer and remove from the list.
+        if(it->second == nullptr){
+            delete (it->second);
+            this->childObjMap.erase(it);
+        }
+        else{
+            //call mouse move
+            if(Layer* childLayer = dynamic_cast<Layer*>((it->second))){
+                if(childLayer->isLayerInputListenable())
+                    childLayer->keyPressed(key, mods);
+            }
+            ++it;
         }
     }
 }
 
 void Scene::keyReleased(int key, int mods){
-    for(auto it : layerMap){
-        if((it.second)->isLayerInputListenable()){
-			(it.second)->keyReleased(key, mods);
+//    for(auto it : layerMap){
+//        if((it.second)->isLayerInputListenable()){
+//			(it.second)->keyReleased(key, mods);
+//        }
+    //    }
+    for (auto it = this->childObjMap.begin(); it != this->childObjMap.end();){
+        //if pointer is null, delete pointer and remove from the list.
+        if(it->second == nullptr){
+            delete (it->second);
+            this->childObjMap.erase(it);
+        }
+        else{
+            //call mouse move
+            if(Layer* childLayer = dynamic_cast<Layer*>((it->second))){
+                if(childLayer->isLayerInputListenable())
+                    childLayer->keyReleased(key, mods);
+            }
+            ++it;
         }
     }
 }
 
 void Scene::mouseButton(double x, double y, int button, int action){
-    for(auto it : layerMap){
-        if((it.second)->isLayerInputListenable()){
-            (it.second)->mouseButton(x, y, button, action);
+//    for(auto it : layerMap){
+//        if((it.second)->isLayerInputListenable()){
+//            (it.second)->mouseButton(x, y, button, action);
+//        }
+    //    }
+    for (auto it = this->childObjMap.begin(); it != this->childObjMap.end();){
+        //if pointer is null, delete pointer and remove from the list.
+        if(it->second == nullptr){
+            delete (it->second);
+            this->childObjMap.erase(it);
+        }
+        else{
+            //call mouse move
+            if(Layer* childLayer = dynamic_cast<Layer*>((it->second))){
+                if(childLayer->isLayerInputListenable())
+                    childLayer->mouseButton(x, y, button, action);
+            }
+            ++it;
         }
     }
 }
 
 void Scene::mouseMove(double x, double y){
-    for(auto it : layerMap){
-        if((it.second)->isLayerInputListenable()){
-            (it.second)->mouseMove(x, y);
+//    for(auto it : layerMap){
+//        if((it.second)->isLayerInputListenable()){
+//            (it.second)->mouseMove(x, y);
+//        }
+//    }
+    for (auto it = this->childObjMap.begin(); it != this->childObjMap.end();){
+        //if pointer is null, delete pointer and remove from the list.
+        if(it->second == nullptr){
+            delete (it->second);
+            this->childObjMap.erase(it);
+        }
+        else{
+            //call mouse move
+            if(Layer* childLayer = dynamic_cast<Layer*>((it->second))){
+                if(childLayer->isLayerInputListenable())
+                    childLayer->mouseMove(x, y);
+            }
+            ++it;
         }
     }
+
 }
--- OpenGL_2D_Framework/Scene.h
+++ OpenGL_2D_Framework/Scene.h
@@ -12,7 +12,8 @@
 //#define GLM_FORCE_RADIANS
 
 #include "CommonInclude.h"
-#include "RenderableObjectManager.h"
+#include "Object.h"
+//#include "RenderableObjectManager.h"
 #include "Layer.h"
 
 #include <map>
@@ -21,22 +22,19 @@
 
 struct GLFWwindow;
 
-class Scene{
+class Scene : public Object{
 protected:
-    RenderableObjectManager* renderableObjectManager;
+//    RenderableObjectManager* renderableObjectManager;
     GLFWwindow* window;
     glm::vec2 prevMousePos;
     glm::vec2 curMousePos;
 private:
-    std::multimap<int/*z order*/, Layer*> layerMap;
     std::map<std::string, bool> inputReceived;
     
 public:
 	Scene();
 	virtual ~Scene();
-//	Scene(Scene const& other);
-//	void operator=(Scene const& other);
-    
+
     bool canMoveCamera;
     
     virtual void init() = 0;
@@ -52,15 +50,15 @@ public:
     virtual void injectKey() = 0;
     virtual void injectMouseMove() = 0;
     
-    RenderableObjectManager* getRenderableObjectManager();
+//    RenderableObjectManager* getRenderableObjectManager();
     
     void run();
     
-    void boundWindow(GLFWwindow* window);
+    void bindWindow(GLFWwindow* window);
     
-    void addLayer(Layer* childLayer);
-    void addObject(std::string objectName, RenderableObject* object);
-    
+//    void addLayer(Layer* childLayer);
+//    void addObject(std::string objectName, RenderableObject* object);
+    void addChild(Object* child);
 };
 
 #endif /* defined(__OpenGL_2D_Framework__Scene__) */
