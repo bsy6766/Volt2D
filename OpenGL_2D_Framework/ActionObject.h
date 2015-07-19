f8062e7b5b6eb7f050392ce42c96b815c417b9a0
bsy6766 <bsy6766@gmail.com>
2015-07-19 00:12:45 -0500

    Added class comment

--- OpenGL_2D_Framework/ActionObject.h
+++ OpenGL_2D_Framework/ActionObject.h
@@ -9,11 +9,14 @@
 #ifndef __OpenGL_2D_Framework__ActionObject__
 #define __OpenGL_2D_Framework__ActionObject__
 
-//#define GLM_FORCE_RADIANS
-
 #include "CommonInclude.h"
 #include "Timer.h"
 #include <glm/glm.hpp>
+
+/**
+ *  @name AcitonObject
+ *  @brief A base class of action classes.
+ */
 
 class RenderableObject;
 
