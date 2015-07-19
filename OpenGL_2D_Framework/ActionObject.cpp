1f513f20c3995514b3b8526085859b3b2962c089
bsy6766 <bsy6766@gmail.com>
2015-07-19 00:11:03 -0500

    Cleaned and commented

--- OpenGL_2D_Framework/ActionObject.cpp
+++ OpenGL_2D_Framework/ActionObject.cpp
@@ -8,15 +8,11 @@
 
 #include "ActionObject.h"
 
-//unsigned int ActionObject::ACTION_ID = 0;
-
 ActionObject::ActionObject():
 running(false),
 alive(true),
-//previousTime(0),
 isProtected(false),
 totalElapsedTime(0),
-speed(1),
 duration(0)
 {
     std::cout << "ActionObject()" << std::endl;
@@ -25,10 +21,7 @@ duration(0)
 ActionObject::ActionObject(const ActionObject& other){
     this->duration = other.duration;
     this->totalElapsedTime = other.totalElapsedTime;
-//    this->previousTime = other.previousTime;
-    this->speed = other.speed;
     this->target = other.target;
-//    this->objID = other.objID;
     this->running = other.running;
     this->alive = other.alive;
     this->isProtected = other.isProtected;
@@ -50,12 +43,10 @@ double ActionObject::setCurrentTime(double elapsedTime){
     tempTime += elapsedTime;
     
     if(tempTime >= duration) {
-//        previousTime = totalElapsedTime;
         totalElapsedTime = duration;
         return (tempTime - duration);
     }
 
-//    previousTime = totalElapsedTime;
     totalElapsedTime += elapsedTime;
     return 0;
 }
@@ -87,8 +78,6 @@ void ActionObject::revive(){
         running = false;
         
         totalElapsedTime = 0;
-//        previousTime = 0;
-        speed = 1;
     }
 }
 
--- OpenGL_2D_Framework/ActionObject.h
+++ OpenGL_2D_Framework/ActionObject.h
@@ -15,28 +15,40 @@
 #include "Timer.h"
 #include <glm/glm.hpp>
 
-//class SpriteObject;
 class RenderableObject;
 
 class ActionObject{
 private:
 protected:
-    //item vars
+    /**
+     *  Action duration
+     */
     double duration;
+    
+    /**
+     *  Action's total elapsed time since it started
+     */
     double totalElapsedTime;
-//    double previousTime;
-    double speed;
     
+    /**
+     *  A target to update from this action
+     */
     RenderableObject* target;
-    
 public:
-//    unsigned int objID;
-    
     ActionObject();
     ActionObject(const ActionObject& other);
     virtual ~ActionObject();
     
+    /**
+     *  Only true if action starts running.
+     *  False means it never started
+     */
     bool running;
+    
+    /**
+     *  Only true if action is valid.
+     *  False means action is ended or dead
+     */
     bool alive;
     
     /**
@@ -45,30 +57,63 @@ public:
      */
     bool isProtected;
     
-    //getters
+    /**
+     *  Getter for action's duration
+     *  @return duration of action object
+     */
     double getDuration();
     
-    bool isRunning();   //check if action object is running or not. 
+    /**
+     *  Checks if action is running or not
+     *  @return true if action is running. Else, false.
+     */
+    bool isRunning();
+    
+    /**
+     *  Checks if action is alive or dead
+     *  @return true if action is still valid. Else, false.
+     */
     bool isAlive();
     
-    //time functions
+    /**
+     *  Update time varaibles of this action object.
+     *  @param elapsedTime An elapsed time for current iteration
+     *  @return Remaining time that action didn't use if it ended. This will be stored and used on next action.
+     */
     double setCurrentTime(double elapsedTime);
     
-    //setters
-    void setSpeed(double speed);
-    
+    /**
+     *  Virtual function for starting action.
+     *  Each action needs to update their own variables so it needs to be overriden
+     */
     virtual void startAction();
-    void stopAction();
     
+    /**
+     *  Update action. Pure virtual. 
+     *  @param remainedTime An reference of remained time that stored in ActionSchedule.
+     */
     virtual void updateAction(double& remainedTime) = 0;
-//    virtual void instantUpdate() = 0;
-//    virtual void intervalUpdate(double& remainedTime) = 0;
     
-    //kill and reset
+    /**
+     *  Kill and stop action. Action will be deleted if it doesn't have to repeat or protected
+     */
     void kill();
+    
+    /**
+     *  Revive this action.
+     */
     virtual void revive();
     
+    /**
+     *  Get target of this object
+     *  @return Object's target
+     */
     RenderableObject* getTarget();
+    
+    /**
+     *  Bind target to update
+     *  @param target A RenderableObject pointer of target
+     */
     void bindTarget(RenderableObject* target);
 };
 
