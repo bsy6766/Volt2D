5b7d4524b13e311bdf1d8605c7e44674ec5b8a0d
bsy6766 <bsy6766@gmail.com>
2015-07-22 05:11:24 -0500

    adding changed resource files

---
+++ OpenGL_2D_Framework/SpriteBatch.cpp
@@ -0,0 +1,9 @@
+//
+//  SpriteBatch.cpp
+//  OpenGL_2D_Framework
+//
+//  Created by Seung Youp Baek on 7/19/15.
+//  Copyright (c) 2015 Seung Youp Baek. All rights reserved.
+//
+
+#include "SpriteBatch.h"
