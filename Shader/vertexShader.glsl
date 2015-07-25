0c01f875d0d3bc9cbc81fbea396ee7568553e800
bsy6766 <bsy6766@gmail.com>
2015-07-25 13:32:13 -0500

    normal sprite shader. Cleaned, removed particle stuffs

--- Shader/vertexShader.glsl
+++ Shader/vertexShader.glsl
@@ -2,7 +2,6 @@
 
 layout(location = 0) in vec3 vert;
 layout(location = 1) in vec2 uvVert;
-layout(location = 2) in vec3 posVert;
 
 uniform mat4 parentMat;
 uniform mat4 modelMat;
@@ -10,25 +9,16 @@ uniform mat4 translateMat;
 uniform mat4 rotateMat;
 uniform mat4 scaleMat;
 uniform mat4 cameraMat;
-uniform bool particle;
 
 out vec2 fragTexCoord;
 
 void main(){
-    vec4 finalPosition;
-    if(particle){
-        //vec3 movedPos = posVert.xyz;
-        vec3 addPos = vert + posVert;
-        finalPosition = vec4(addPos, 1.0f);
-    }
-    else{
-        finalPosition = vec4(vert, 1.0f);
-    }
+    vec4 finalPosition = vec4(vert, 1.0f);
+    
     //object's mat.
     //Transformation order.
     //From parent -> own model matrix -> own transformation
     mat4 objectMat = parentMat * translateMat * rotateMat * scaleMat * modelMat;
-//    mat4 objectMat = modelMat * rotateMat * translateMat * scaleMat;
     gl_Position = cameraMat * objectMat * finalPosition;
     fragTexCoord = uvVert;
 }
