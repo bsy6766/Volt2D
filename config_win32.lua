6a1a2e08d356a60a5812ca62ceb742570e5c12a3
Seung Youp Baek <bsy6766@gmail.com>
2015-07-27 12:50:48 -0500

    config for win32

---
+++ config_win32.lua
@@ -0,0 +1,25 @@
+system = {
+	window = {
+		size = {
+	    	screenWidth = 1920,
+	   		screenHeight = 1080
+		},
+		title = "JB_Engine Test Build",
+		fullscreen = false,
+	 	windowed = true,
+	 	borderless = true
+	},
+	clearBuffer = {
+		r = 0,
+		g = 0,
+		b = 0
+	},
+	versionStr = "1.0b",
+	resourcePath = {
+		-- Resource path from working directory. 
+		texture = "/../Texture/",
+		shader = "/../Shader/",
+		font = "/../Font/",
+		sound = "/../Sound/"
+	}
+}
