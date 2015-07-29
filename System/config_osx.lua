bff132418c430e6332c325d8eeb99f965ea3b499
bsy6766 <bsy6766@gmail.com>
2015-07-28 20:22:10 -0500

    adding system folder. All system related files will go here(like default sutffs)

---
+++ System/config_osx.lua
@@ -0,0 +1,25 @@
+system = {
+	window = {
+		size = {
+	    	screenWidth = 1440,
+	   		screenHeight = 900
+	    	-- screenWidth = 1280,
+	   		-- screenHeight = 720
+		},
+		title = "JB_Engine Test Build",
+		fullscreen = false,
+	 	borderless = true,
+	 	captureMouse = true,
+	 	cursorHidden = false,
+	 	vsync = true
+	},
+
+	versionStr = "1.0b",
+	resourcePath = {
+		-- Resource path from working directory. 
+		texture = "/../Texture/",
+		shader = "/../Shader/",
+		font = "/../Font/",
+		sound = "/../Sound/"
+	}
+}
