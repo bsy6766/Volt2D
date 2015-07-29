88ce95d501e854724594aa5124112528e16abd7f
bsy6766 <bsy6766@gmail.com>
2015-07-28 21:27:01 -0500

    Separating files into folders by same type

--- Volt2D/ActionEaseIn.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ActionEaseIn.cpp
-//  OpenGL_2D_Framework
-//
-//  Created by Seung Youp Baek on 6/15/15.
-//  Copyright (c) 2015 Seung Youp Baek. All rights reserved.
-//
-
-#include "ActionEaseIn.h"
--- Volt2D/SpriteBatch.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  SpriteBatch.cpp
-//  OpenGL_2D_Framework
-//
-//  Created by Seung Youp Baek on 7/19/15.
-//  Copyright (c) 2015 Seung Youp Baek. All rights reserved.
-//
-
-#include "SpriteBatch.h"
