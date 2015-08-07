c82296bda3d78f88f824d28d79203f8b12aabe71
bsy6766 <bsy6766@gmail.com>
2015-08-06 21:43:45 -0500

    Fixed particle shader to support more transformations

--- Shader/particleVertexShader.glsl
+++ Shader/particleVertexShader.glsl
@@ -3,14 +3,15 @@
 layout(location = 0) in vec3 vert;
 layout(location = 1) in vec2 uvVert;
 layout(location = 2) in vec3 posVert;
-layout(location = 3) in vec4 particleColor;
+layout(location = 3) in vec3 spawnPosVert;
+layout(location = 4) in vec4 particleColor;
 /**
  *  x = [0][0]
  *  y = [0][1]
  *  z = [1][0]
  *  w = [1][1]
  */
-layout(location = 4) in vec4 particleTransform;
+layout(location = 5) in vec4 particleTransform;
 
 uniform mat4 parentMat;
 uniform mat4 modelMat;
@@ -33,15 +34,24 @@ void main(){
     particleScaleMat[3][3] = 1.0f;
     
     vec4 finalPosition;
-    vec4 vec4vert = vec4(vert, 1.0f);
-    vec4vert = particleScaleMat * vec4vert; //matrix must come first than vector
-    vec4vert += vec4(posVert, 0.0f);    //w must be 1 after addition
-    finalPosition = vec4vert;
-    //object's mat.
-    //Transformation order.
-    //From parent -> own model matrix -> own transformation
-    mat4 objectMat = parentMat * translateMat * rotateMat * scaleMat * modelMat;
-    gl_Position = cameraMat * objectMat * finalPosition;
+    //conver to vec4
+    vec4 vert4 = vec4(vert, 1.0f);
+    vec4 posVert4 = vec4(posVert, 1.0f);
+    vec4 spawnPosVert4 = vec4(spawnPosVert, 1.0f);
+    //first, rotate and scale particle itself on origin
+    vert4 = particleScaleMat * vert4;
+    //then, move to particle's position (from spwan, but adding up early on origin)
+    vert4 += posVert4;
+    vert4.w = 1.0f;
+    //then, apply particle system's scale and rotation mat
+    vert4 = rotateMat * scaleMat * vert4;
+    //then, translate by spawn distance from origin
+    vert4 += spawnPosVert4;
+    vert4.w = 1.0f;
+    //set to final position
+    finalPosition = vert4;
+    //apply parent and translate mat and set gl position
+    gl_Position = cameraMat * parentMat * finalPosition * translateMat * modelMat;
     fragTexCoord = uvVert;
     pColor = particleColor;
 }
