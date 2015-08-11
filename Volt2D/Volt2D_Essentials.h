4224bf26ed6458fcaba4cc9ca5cfc54dfc498b38
bsy6766 <bsy6766@gmail.com>
2015-08-11 16:01:57 -0500

    Header pack

---
+++ Volt2D/Volt2D_Essentials.h
@@ -0,0 +1,21 @@
+//
+//  Volt2D_Essentials.h
+//  Volt2D
+//
+//  Created by Seung Youp Baek on 8/10/15.
+//  Copyright (c) 2015 Seung Youp Baek. All rights reserved.
+//
+
+#ifndef Volt2D_Volt2D_Essentials_h
+#define Volt2D_Volt2D_Essentials_h
+
+#include "Sprite.h"
+#include "SpriteAnimation.h"
+#include "ProgressBar.h"
+#include "ProgressRadian.h"
+#include "Text.h"
+#include "Transition.h"
+#include "Director.h"
+#include "Actions.h"
+
+#endif
